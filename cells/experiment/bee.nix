c8f5626fe8df3ca18fafbf17a89200b2189c8456
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-10 20:52:31 +0900

    feat: use hive(collect part in the tutorial)

---
+++ cells/experiment/bee.nix
@@ -0,0 +1,5 @@
+{ inputs, cell }:
+{
+  system = "x86_64-linux";
+  pkgs = inputs.nixpkgs;
+}
--- flake.nix
+++ flake.nix
@@ -1,30 +1,40 @@
 {
   outputs =
-    { ... }@inputs:
     {
-      nixosConfigurations.experiment = inputs.nixpkgs-stable.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          (
-            { pkgs, ... }:
-            {
-
-              users.users.foo = {
-                isNormalUser = true;
-                initialPassword = "foo";
-              };
-
-              environment.systemPackages = with pkgs; [ hello ];
-              system.stateVersion = "23.11";
-            }
-          )
-        ];
+      std,
+      hive,
+      self,
+      ...
+    }@inputs:
+    hive.growOn
+      {
+        inherit inputs;
+        cellsFrom = ./cells;
+        cellBlocks =
+          with hive.blockTypes;
+          with std.blockTypes;
+          [
+            nixosConfigurations
+            (functions "bee")
+          ];
+      }
+      {
+        nixosConfigurations = hive.collect self "nixosConfigurations";
       };
-    };
 
   inputs = {
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
     nixpkgs.follows = "nixpkgs-unstable";
+
+    std = {
+      url = "github:divnix/std";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hive = {
+      url = "github:divnix/hive";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
