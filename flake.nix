38319fd5649b4874002d2c06d329b581cf241c66
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-10 21:04:11 +0900

    feat: use findLoad(findLoad in the tutorial)

--- flake.nix
+++ flake.nix
@@ -24,8 +24,7 @@
 
   inputs = {
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
-    nixpkgs.follows = "nixpkgs-unstable";
+    nixpkgs.follows = "nixpkgs-stable";
 
     std = {
       url = "github:divnix/std";
