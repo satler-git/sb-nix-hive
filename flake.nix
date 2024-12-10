e9005229ed8247e881984137e00de28513ef36e8
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-10 20:23:07 +0900

    feat: init flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  outputs =
+    { ... }@inputs:
+    {
+      nixosConfigurations.experiment = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (
+            { pkgs, ... }:
+            {
+
+              users.users.foo = {
+                isNormalUser = true;
+                initialPassword = "foo";
+              };
+
+              environment.systemPackages = with pkgs; [ hello ];
+              system.stateVersion = "23.11";
+            }
+          )
+        ];
+      };
+    };
+
+  inputs = {
+    nixpkgs-stable.url = "github:nixos/nixpkgs/24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
+    nixpkgs.follows = "nixpkgs-unstable";
+  };
+}
